afe70cb8b3417a230e3d9fbcbc022d1a818d9eda
LuizaF18 <luiza.felicia18@yahoo.it>
2023-05-21 11:59:37 +0200

    Write first draft of a pseudo-code for the game

---
+++ java.js
@@ -0,0 +1,16 @@
+// write a function with two parameters, computer selection and player selection
+//computer has to randomly choose between rock or paper or scissors
+//player can choose between rock or paper or scissors
+//if rock and paper, paper wins
+//if rock and scissors, rock wins
+//if paper and scissors, scissors win
+//at the end of choice, return a string that declares the winner of the round
+//player inputs string, make string case insensitive
+//use prompt to get input from user
+
+//write a game function
+//it has to loop for 5 times and keep score of the wins
+//use console log to display the results at the end of each round
+//if computer gets to 5, computer wins
+//if player gets to 5, player wins
+//use console log to declare the winner
