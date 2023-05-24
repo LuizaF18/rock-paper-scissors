3c7bfee3a4f57e000b92177ecb8864c0f62ec973
LuizaF18 <luiza.felicia18@yahoo.it>
2023-05-24 15:04:46 +0200

    Complete a round of the game

--- java.js
+++ java.js
@@ -1,11 +1,42 @@
-// write a function with two parameters, computer selection and player selection
+const choices = ["rock", "paper", "scissors"]
+
 //computer has to randomly choose between rock or paper or scissors
 function getComputerChoice() {
-    const arr = ["Rock", "Paper", "Scissors"]
-    const randomIndex = Math.floor(Math.random() * arr.length)
-    return randomItem = arr[randomIndex]
+    const randomIndex = Math.floor(Math.random() * choices.length)
+    return randomItem = choices[randomIndex]
 }
+
 //player can choose between rock or paper or scissors
+
+// write a function with two parameters, computer selection and player selection
+
+function playRound(playerSelection, computerSelection) {
+    computerSelection = getComputerChoice()
+    if (computerSelection == "rock" && playerSelection == "scissors") {
+       return "You lose! Rock beats scissors.";
+      } else if (computerSelection == "rock" && playerSelection == "paper") {
+       return "You win! Paper beats rock.";
+      } else if (computerSelection == playerSelection) {
+       return "It's a tie!";
+     } else if (computerSelection == "paper" && playerSelection == "scissors") {
+       return "You win! Scissors beats paper.";
+        } else if (computerSelection == "paper" && playerSelection == "rock") {
+       return "You lose! Paper beats rock.";
+       } else if (computerSelection == "scissors" && playerSelection == "paper") {
+       return "You lose! Scissors beats paper.";
+      } else if (computerSelection == "scissors" && playerSelection == "rock") {
+       return "You win! Rock beats scissors.";
+      }
+}
+
+
+
+
+
+
+
+
+
 //if rock and paper, paper wins
 //if rock and scissors, rock wins
 //if paper and scissors, scissors win
