76342399624d667e34fea724c7e3268cbdc54d14
LuizaF18 <luiza.felicia18@yahoo.it>
2023-05-25 17:11:06 +0200

    Get rid of redundant code

--- java.js
+++ java.js
@@ -1,4 +1,6 @@
 const arr = ["rock", "paper", "scissors"]
+let playerScore = 0
+let computerScore = 0
 
 function getComputerChoice() {
     let random = [Math.floor(Math.random() * arr.length)]
@@ -6,53 +8,31 @@ function getComputerChoice() {
 }
 
 function getPlayerChoice() {
-    let input = prompt("Rock, paper or scissors?");
-    input = input.toLowerCase()
-    while (input !== arr) {
-        return prompt("Rock, paper, or scissors.")
-    }
+    let input = prompt("Rock, paper or scissors?", "rock").toLowerCase();
     return input
 }
 
-function checkWinner(playerSelection, computerSelection) {
+
+function playRound(playerSelection, computerSelection) {
+    console.log(playerScore, computerScore)
     if (playerSelection == computerSelection) {
-        return "Tie"
+        return `It's a tie! You both picked ${playerSelection}`
     } else if (playerSelection == "rock" && computerSelection == "scissors" || 
     playerSelection == "paper" && computerSelection == "rock" ||
     playerSelection == "scissors" && computerSelection == "paper") {
-        return "Player"
-    } else {
-        return "Computer"
-    }
-}
-
-function playRound(playerSelection, computerSelection) {
-    computerSelection = getComputerChoice()
-    playerSelection = "rock"
-    if (checkWinner(playerSelection, computerSelection) == "Tie") {
-        return "It's a tie!"
-    } else if (checkWinner(playerSelection, computerSelection) == "Player") {
+        playerScore++
         return `You win! ${playerSelection} beats ${computerSelection}.`
-    } else if (checkWinner(playerSelection, computerSelection) == "Computer") {
+    } else {
+        computerScore++
         return `You lose! ${computerSelection} beats ${playerSelection}.`
     }
 }
 
 function game() {
-    let playerScore = 0
-    let computerScore = 0
-    for (i = 0; i < 5; i++) {
-        computerSelection = getComputerChoice()
-        playerSelection = "rock"
-        playRound(playerSelection, computerSelection)
+    for (let i = 0; i < 5; i++) {
+        const computerSelection = getComputerChoice()
+        const playerSelection = getPlayerChoice()
         console.log(playRound(playerSelection, computerSelection))
-        if (checkWinner(playerSelection, computerSelection) == "Player") {
-            playerScore++
-        } else if (checkWinner(playerSelection, computerSelection) == "Computer") {
-            computerScore++
-        } else if (checkWinner(playerSelection, computerSelection) == "Tie") {
-            playerScore++ && computerScore++
-        }
     }
     console.log("Game Over!")
     if (playerScore > computerScore) {
